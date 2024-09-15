cd06eae44f3a859cbacc295d9373cbe4901921db
Sandaruwan Wijerathne <sandaruwanwijerathne9495@gmail.com>
2024-09-16 01:21:51 +0530

    Import path to resolve build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
 const nextConfig = {
   webpack: function (config, env) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     config.resolve.fallback = {
       fs: false,
       path: false,
