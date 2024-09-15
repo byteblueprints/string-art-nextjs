82cbd70a74f64b5fd51d7d7359d206872ccb8138
Sandaruwan Wijerathne <sandaruwanwijerathne9495@gmail.com>
2024-09-16 01:24:27 +0530

    Fix __dirname is not defined

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-import path from "path";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   webpack: function (config, env) {
     config.resolve.alias["@"] = path.resolve(__dirname);
